ed8acc85afc703534c4e76d6bd16149e6196559d
Zilvinas Kybartas <zhilvis@inventi.lt>
2018-12-09 23:24:38 +0200

    add instructions

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #%/bin/bash
 
 docker run -it -v $(pwd):/home/boot/repo docker.io/zhilvis/boot-base bash -c "cd repo;boot build"
+docker build .
 
 #heroku container:login
 #docker tag <img> registry.heroku.com/swed-ynab/web
 #docker push registry.heroku.com/swed-ynab/web
+#heroku container:release web -a swed-ynab
